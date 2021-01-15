63ba5597dd47fb267616eb49f0a51fca237a74bd
Bangmek1999 <bangmek1999@gmail.com>
2021-01-15 16:34:06 +0700

    UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 @section('content')
-<a   class="btn btn-secondary" href="{{ route('dashboard') }}">DashBoard</a>
-
+<a class="btn btn-secondary" href="{{ route('dashboard') }}">DashBoard</a>
 <div class="container">
     <h3>Animation</h3>
     <div class="col-lg-5 col-md-4 col-sm-4 col-xs-12">
-    <img src='https://i.pinimg.com/originals/e8/88/d4/e888d4feff8fd5ff63a965471a94b874.gif' class="card-img-top" width="300" height="500">
-</div>
+        <img src='https://i.pinimg.com/originals/e8/88/d4/e888d4feff8fd5ff63a965471a94b874.gif' class="card-img-top" width="300" height="500">
+    </div>
     <!-- <div class="card-center" style="width: 20rem;">
         <div class="card-body">
             <table class="table table-brodered table-striped">
